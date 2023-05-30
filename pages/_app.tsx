3fdce72791e8262dc6df0731cd49ad44c36aa242
gioeste <61016188+gioeste@users.noreply.github.com>
2023-05-30 18:11:16 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mumbai;
+const activeChainId = ChainId.Polygon;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
