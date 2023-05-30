3507fdb3ca2c7b28f257c97da405eaf9cb92f444
gioeste <61016188+gioeste@users.noreply.github.com>
2023-05-30 18:12:43 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ import styles from "../styles/Home.module.css";
 import { parseIneligibility } from "../utils/parseIneligibility";
 
 const Home = () => {
-  const tokenAddress = "0x03728725240b021887355c943d040BF933F3d5F0";
+  const tokenAddress = "0x286ff51c8fA78Df00ffE433Cf0dB45c3fB114570";
   const { contract } = useContract(tokenAddress, "token-drop");
   const address = useAddress();
   const [quantity, setQuantity] = useState(1);
